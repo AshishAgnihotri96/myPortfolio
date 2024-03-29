a592a3196ca874c7e38aebc4670e5047ad882011
Ashish Agnihotri <agnih96@gmail.com>
2024-03-29 23:44:21 +0530

    demo url change

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -27,7 +27,7 @@ const Projects = () => {
               title="The Ink Shop"
               description="The Printers Website is a modern and user-friendly platform designed for users to explore various types of printers and make purchases conveniently. Whether you're a home user, student, or business professional, our website offers a wide range of printers to suit your needs."
               ghLink="https://github.com/AshishAgnihotri96/theinkshop.git"
-              demoLink="theinkshop.vercel.app/"
+              demoLink="https://theinkshop.vercel.app/"
             />
           </Col>
 
@@ -38,7 +38,7 @@ const Projects = () => {
               title="Password Generator"
               description="The Password Generator is a web application that allows users to generate secure passwords with customizable options. It provides a convenient way to create strong passwords for various online accounts, enhancing security and protecting user data."
               ghLink="https://github.com/AshishAgnihotri96/passwordGenerator.git"
-              demoLink="fervent-volhard-f2991a.netlify.app/"
+              demoLink="https://fervent-volhard-f2991a.netlify.app/"
             />
           </Col>
 
@@ -49,7 +49,7 @@ const Projects = () => {
               title="Movie App"
               description="The Movie Search and Watch App is a web application that allows users to search for movies, view details, and stream them online. With a vast database of movies, users can discover new releases, browse by genre, and enjoy their favorite films from the comfort of their home."
               ghLink="https://github.com/AshishAgnihotri96/moviesApp.git"
-              demoLink="entertainmenthub.netlify.app/"
+              demoLink="https://entertainmenthub.netlify.app/"
             />
           </Col>
         </Row>
